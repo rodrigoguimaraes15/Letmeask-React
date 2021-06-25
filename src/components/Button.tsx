b99b87eb6aebebf92425880aafe2a859d3eb32b1
Rodrigo Guimarães <rodrigo.silvaguimaraes1992@gmail.com>
2021-06-25 18:44:16 -0300

    commit inicial aula 4

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes } from 'react'
+
+import '../style/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean
+};
+
+export function Button({ isOutlined = false, ...props}: ButtonProps) {
+     
+    return (
+        <button 
+            className={`button ${isOutlined ? 'outLined' : ''}`}
+            {...props} 
+        />    
+    )  
+}
+
